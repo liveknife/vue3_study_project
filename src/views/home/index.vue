9c0af7ef3b5b6af2131ebeacfd1cb45ae67da05c
MaKun <MaKunn@163.com>
2023-06-21 20:31:49 +0800

    update

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,27 @@
 <template>
-    <div>
-        首页
-    </div>
+  <div>
+    首页
+    <button @click="login">Login</button>
+  </div>
+  <canvas></canvas>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+import canvas from '../canvas/canvas.vue';
+// import { Debounce, Throttler } from '../../utils/common';
+// import { Debounce, Throttle } from '../../utils/common'
+// 创建防抖实例
+// const debounceInstance = new Debounce(0, 1000);
 
+// 创建节流实例
+// const throttleInstance = new Throttler(0, 1000);
+// console.log(debounceInstance)
+// console.log(throttleInstance)
+const router = useRouter()
+const login = () => {
+  router.push('/login')
+}
 </script>
 
 <style scoped lang="scss"></style>
